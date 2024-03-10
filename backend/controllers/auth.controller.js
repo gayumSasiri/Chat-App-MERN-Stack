5eb8de1c11fc311b4f5245a503d0c677bf75ef5f
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2024-03-10 08:28:07 +0530

    password hashed implemented before save in db

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 
 export const signup = async (req,res) => {
@@ -8,9 +9,46 @@ export const signup = async (req,res) => {
             return res.status(400).json({error:"Passwords don't match"})
         }
 
-        const user = await User.findOne({username})
-    } catch (error) {
+        const user = await User.findOne({username});
 
+        if(user) {
+            return res.status(400).json({error:"Username already exists"});
+        }
+
+        //Hash password here
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User ({
+            fullname,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        if(newUser){
+            //generate JWT token here
+            
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username,
+                profilePic : newUser.profilePic
+            });
+        } else {
+            res.status(400).json({error: "Invalid user data"});
+        }
+
+    } catch (error) {
+        console.log("Error in signUp", error.message);
+        res.status(500).json({error: "Internl server Error"});
     }
 };
 
